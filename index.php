2bb274db7655ea76ce1cf1456cc07cc62c3abd85
maxime blanc <maxime.blanc@deindeal.ch>
2019-12-17 15:08:39 +0100

    create small template engine

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ function main() {
     return "Bonjour tout le monde";
 }
 
+$regex = "/{{\s?(\w+)\s?}}/";
+
 $pagecontents = file_get_contents("./index.html");
 
-echo str_replace("\$myVar", "Pineapple", $pagecontents);
+$vars = [
+    "title" => "Bienvenue sur mon site",
+    "content" => "Lorem ipsum dolor site amet"
+];
+
+echo preg_replace_callback($regex, function ($match) use ($vars) {
+    return strtr($match[1], $vars);
+}, $pagecontents);
